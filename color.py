0a32d28187da97009490cfd95f99611db7e51199
Ayden <ayden@microwavebot.tech>
2022-06-17 09:20:50 -0700

    Update color.py
    Added new values

--- color.py
+++ color.py
@@ -1,12 +1,13 @@
 import requests
 import random
 
-a = random.randint(0,255)
-b = random.randint(0,255)
-c = random.randint(0,255)
-color = f"{a}, {b}, {c}"
+a = random.randint(0,255) #Red.
+b = random.randint(0,255) #Green.
+c = random.randint(0,255) #Blue.
+color = f"{a}, {b}, {c}" #RGB values all together.
 r = requests.get(f"https://www.thecolorapi.com/id?rgb={color}")
-colorname = r.json()["name"]['value']
 
-print(f"Your color's rgb value is: R:{a},G:{b},B:{c}")
-print(f"Your color's name is: {colorname}")
+print("Name: " + r.json()["name"]['value']) #Prints the name
+print("Hex: " + r.json()["hex"]["value"]) #Prints the hex code
+print(f"Red: {a}, Green: {b}, Blue: {c}") #Prints the RGB values
+print("Image: " + r.json()["image"]["named"]) #Prints the link to the image
